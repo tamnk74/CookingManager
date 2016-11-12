5d2ca9aada309d06427b4b7c0fe97ac2d83158fd
khactam94 <khac.tam.94@gmail.com>
2016-11-12 17:31:57 +0700

    file chua gia tri mac dinh he thong

---
+++ src/Config/Const.java
@@ -0,0 +1,7 @@
+package Config;
+
+public class Const {
+	public static final int MAX_LENGTH_USERNAME = 30;
+	public static final int MAX_LENGTH_PASSWORD = 30;
+	public static final int MAX_LENGTH_FULLNAME = 50;
+}
